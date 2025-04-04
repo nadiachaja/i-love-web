e149898208f695d587066b6f03eaa17abd2ee805
nadiachaja <nadiagaja13@gmail.com>
2025-04-04 15:15:44 +0200

    liquid

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
 // Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
-import express from 'express'
+import express, { response } from 'express'
 
+// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
+import { Liquid } from 'liquidjs';
 
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
 const app = express()
@@ -10,8 +12,28 @@ const app = express()
 // Bestanden in deze map kunnen dus door de browser gebruikt worden
 app.use(express.static('public'))
 
+// Stel Liquid in als 'view engine'
+const engine = new Liquid();
+app.engine('liquid', engine.express()); 
+
+// Stel de map met Liquid templates in
+// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
+app.set('views', './views')
+
 // Zorg dat werken met request data makkelijker wordt
 app.use(express.urlencoded({extended: true}))
+
+app.get('/', async function(request, response){
+    response.render('index.liquid')
+    })
+
+app.get('/basic', async function(request, response){
+response.render('basic.liquid')
+})
+
+app.get('/input', async function(request, response){
+    response.render('input.liquid')
+    })
 
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
